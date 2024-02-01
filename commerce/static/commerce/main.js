452816aa2bc743e8a0068120123d02c041c2815d
tahzeebahmed99 <tahzeeb2g@gmail.com>
2024-02-01 16:11:04 +0600

    transfered most script codes to main.js and styles.css from auction app to project level access

--- commerce/static/commerce/main.js
+++ commerce/static/commerce/main.js
@@ -1,3 +1,325 @@
-$(document).ready(function () {
-    console.log("main.js")
-})
+const customSuccessColor = getComputedStyle(document.documentElement).getPropertyValue('--custom-success');
+const customDarkColor = getComputedStyle(document.documentElement).getPropertyValue('--custom-dark');
+const customInfoColor = getComputedStyle(document.documentElement).getPropertyValue('--custom-primary');
+const customDangerColor = getComputedStyle(document.documentElement).getPropertyValue('--custom-danger');
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    (function () {
+        attachLoaderOnClick();
+        handleCategoryLinks();
+    })();
+
+    if (document.querySelector('#listings-section')) {
+        handleFiltering();
+    }
+
+    // Check if the payment elements exist on the page before invoking the function
+    if (document.querySelector('#payment-form')) {
+        handlePaymentOptions();
+    }
+});
+
+
+function attachLoaderOnClick() {
+    document.addEventListener('click', function (event) {
+        if (event.target.classList.contains('loader-on-click')) {
+            var button = event.target;
+            button.disabled = true;
+
+            var form = button.closest('form');
+            if (form) {
+                var hiddenInput = document.createElement('input');
+                hiddenInput.type = 'hidden';
+                hiddenInput.name = 'loading_indicator';
+                hiddenInput.value = 'true';
+                form.appendChild(hiddenInput);
+
+                // Attach a listener to the form's submit event
+                form.addEventListener('submit', function () {
+                    // Reset the button's state and content upon form submission
+                    button.disabled = false;
+                    button.innerHTML = 'Submit'; // Replace with the original content
+                });
+            }
+
+            button.innerHTML = `
+                <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                Loading...
+            `;
+
+            if (form) {
+                form.submit();
+            }
+        }
+    });
+
+    // Attach a listener to the window's unload event
+    window.addEventListener('unload', function () {
+        // Reset the button's state and content when the user navigates away from the page
+        var buttons = document.querySelectorAll('.loader-on-click');
+        buttons.forEach(function (button) {
+            button.disabled = false;
+            button.innerHTML = 'Submit'; // Replace with the original content
+        });
+    });
+}
+
+
+function handleCategoryLinks() {
+    var categoryLinks = document.querySelectorAll('.category-link');
+    categoryLinks.forEach(function (link) {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+            var categoryValue = this.getAttribute('data-category');
+            handleCategoryChange(categoryValue);
+        });
+    });
+}
+
+
+function handleFiltering() {
+    var baseUrl = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ":" + window.location.port : "") + "/filter/";
+    var currentPage = 1
+    var delay = 500;
+    var rangeTimeout;
+    const mobileScreenWidth = 768;
+    var sortOption = 'best-match'
+
+
+    function attachSortOptionListener() {
+        $('.sort-option').click(function () {
+            sortOption = $(this).data('sort');
+            updateListings();
+        });
+    }
+
+    // Show/hide the scroll-to-top button based on scroll position
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('#scroll-to-to-btn').fadeIn();
+        } else {
+            $('#scroll-to-to-btn').fadeOut();
+        }
+    });
+
+    // Scroll to top when the button is clicked
+    $('#scroll-to-to-btn').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 200);
+        return false;
+    });
+
+    // Handle checkbox change event
+    $('.filter-checkbox').change(updateListings);
+
+    $('#mobile_price_range').on('input', function () {
+        // Clear the previous timeout
+        clearTimeout(rangeTimeout);
+        // Set a new timeout to call the updateListings function after the specified delay
+        rangeTimeout = setTimeout(updateListings, delay);
+    });
+
+
+    // Handle range input change event
+    $('#desktop_price_range').on('input', function () {
+        // Clear the previous timeout
+        clearTimeout(rangeTimeout);
+        // Set a new timeout to call the updateListings function after the specified delay
+        rangeTimeout = setTimeout(updateListings, delay);
+    });
+
+    $(document).on('click', '.page-link', function () {
+        var val = $(this).data('label');
+        if (val == "previous") {
+            if (currentPage > 1) {
+                currentPage = currentPage - 1;
+            }
+        }
+        else if (val == "next") {
+            currentPage = currentPage + 1;
+        }
+
+        else {
+            currentPage = val;
+        }
+        updateListings();
+
+    });
+
+    // Handle remove filter click event
+    $(document).on('click', '.remove-filter', function () {
+        var labelToRemove = $(this).data('label');
+        $('.filter-checkbox[data-filter-type="' + labelToRemove + '"]').prop('checked', false);
+
+        // Trigger the updateListings function
+        updateListings();
+    });
+
+    attachSortOptionListener();
+
+    function updateListings() {
+        var selectedFilters = {
+            'page': currentPage,
+            'sort': sortOption
+        };
+
+        // Collect selected filters
+        $('.filter-checkbox:checked').each(function () {
+            var filterType = $(this).data('filter-type');
+            var filterValue = $(this).val();
+            if (!selectedFilters.hasOwnProperty(filterType)) {
+                selectedFilters[filterType] = [];
+            }
+
+            // Add the filter value to the array
+            selectedFilters[filterType].push(filterValue);
+        });
+
+        var rangeValue = $('#desktop_price_range').val();
+
+        if (window.innerWidth < mobileScreenWidth) {
+            var rangeValue = $('#mobile_price_range').val();
+        }
+        selectedFilters['price_range'] = [rangeValue];
+
+        // Construct the URL with query parameters
+        var dynamicUrl = baseUrl + "?" + $.param(selectedFilters);
+        // Send AJAX request to update listings
+        $.ajax({
+            type: 'GET',
+            url: dynamicUrl,
+            success: function (data) {
+                $('#listings-container').html(data);
+                attachSortOptionListener();
+            },
+            error: function () {
+                console.error('Error occurred while updating listings.');
+            }
+        });
+    }
+
+}
+
+function confirmCounterOffer() {
+    Swal.fire({
+        title: 'Accept Offer?',
+        icon: 'success',
+        showCancelButton: true,
+        confirmButtonColor: customSuccessColor,
+        cancelButtonColor: customDarkColor,
+        confirmButtonText: 'Yes, accept offer!',
+        cancelButtonText: 'Go Back'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            // Get all elements with the class name 'counter-offer-btn'
+            document.getElementById('counter-offer-table').innerHTML = `
+                    <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                    Loading...
+                `;
+        }
+    });
+}
+function rejectCounterOffer() {
+    Swal.fire({
+        title: 'Reject Offer?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: customSuccessColor,
+        cancelButtonColor: customDarkColor,
+        confirmButtonText: 'Yes, Reject offer.',
+        cancelButtonText: 'Go Back'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            // Get all elements with the class name 'counter-offer-btn'
+            document.getElementById('counter-offer-table').innerHTML = `
+                    <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                    Loading...
+                `;
+        }
+    });
+}
+
+function confirmShippingAddressDelete(addressId) {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: 'You won\'t be able to revert this!',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: customDangerColor,
+        cancelButtonColor: customDarkColor,
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            // User clicked "Yes, delete it!" - perform the deletion action
+            window.location.href = "{% url 'remove-shipping-address' 0 %}".replace('0', addressId);
+        }
+    });
+}
+
+function confirmPaymentMethodDelete(methodId) {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: 'You won\'t be able to revert this!',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: customDangerColor,
+        cancelButtonColor: customDarkColor,
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            window.location.href = "{% url 'remove-payment-method' 0 %}".replace('0', methodId);
+        }
+    });
+}
+
+
+function confirmProfileChange() {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: 'Email/Password change process will deactivate your account till you have verified it.',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: customSuccessColor,
+        cancelButtonColor: customDarkColor,
+        confirmButtonText: 'Yes, change it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            // Perform the action if the user confirms
+            document.getElementById('email-form-button').innerHTML = `
+                <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                Loading...
+            `;
+            document.getElementById('email-form').submit();
+        }
+    });
+}
+
+function handlePaymentOptions() {
+    var paymentOptionSelect = document.querySelector('#id_payment_option');
+    var cardPaymentDiv = document.querySelector('#card-payment');
+    var bkashPaymentDiv = document.querySelector('#bkash-payment');
+
+    // Initial toggle based on selected payment option
+    togglePaymentForms();
+
+    // Event listener for changes in the payment option
+    paymentOptionSelect.addEventListener('change', togglePaymentForms);
+
+    function togglePaymentForms() {
+        var selectedOption = paymentOptionSelect.value;
+
+        // Toggle visibility based on selected payment option
+        cardPaymentDiv.style.display = (selectedOption === 'CC' || selectedOption === 'DC') ? 'block' : 'none';
+        bkashPaymentDiv.style.display = (selectedOption === 'B') ? 'block' : 'none';
+
+        // Add or remove the 'required' attribute based on visibility
+        Array.from(cardPaymentDiv.querySelectorAll('input')).forEach(function (input) {
+            input.required = (selectedOption === 'CC' || selectedOption === 'DC');
+        });
+
+        Array.from(bkashPaymentDiv.querySelectorAll('input')).forEach(function (input) {
+            input.required = (selectedOption === 'B');
+        });
+    }
+}
